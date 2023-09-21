925d2873458cdf62e5652dca3ace7127229b8ffa
BT_2N2_02 <sample@bths.edut>
2023-09-21 13:37:51 -0400

    fihnal

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -4,7 +4,8 @@ public class TipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("How many people are eating? ");
+        System.out.println("Welcome to the tip calculator!");
+        System.out.print("How many people are in your group? ");
         int numPeople = scan.nextInt();
         System.out.print("What are the costs for items you ordered? If you are done, type -1: ");
         double costs = scan.nextDouble();
@@ -16,13 +17,21 @@ public class TipCalculator {
             costs = scan.nextDouble();
         }
 
-        System.out.print("How much percent do you want to tip? ");
+        System.out.print("What is the tip percentage? (0-100): ");
         int tip = scan.nextInt();
         double aIT = total * (tip/100.0);
         double perPerson = aIT/numPeople;
-        perPerson = Math.round(perPerson * 100)/100;
+        perPerson = (int)((perPerson + 0.5) * 100)/100.0;
+        double costPPBT = total/numPeople;
 
-        System.out.println("Total bill ");
-        System.out.print("Each person has to tip " + (perPerson) + "$.");
+
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("Total bill before tip: " + (total) + "$");
+        System.out.println("Total percentage: " + tip + "$");
+        System.out.println("Total tip: " + aIT + "$");
+        System.out.println("Total bill with tip: " + (aIT + total) + "$");
+        System.out.println("Per person cost before tip: " + costPPBT + "$");
+        System.out.println("Each person has to tip " + (perPerson) + "$.");
+        System.out.println("Total cost per person: " + (perPerson + costPPBT) + "$");
     }
 }
